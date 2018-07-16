d7f5b11eb5cd843239ddf7d5eff0a39c712b886c
Jesse Drebin <jessedrebin@gmail.com>
2018-07-16 03:12:05 -0700

    add jquery drills

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,20 @@
-$(document).ready(function(){
-
-
-
+$( document ).ready(function() {
+  $("form").submit(function () {
+    let msg = $('#message').val();
+    if (msg.length === 0) {
+      $("#btn").prop("disabled", true);
+    }
+    else {
+      alert(msg);
+    }
+  // });
+  $('<div></div>').appendTo('body');
   
-});
+  
+  $('form').submit(function(event){
+    event.preventDefault();
+    let msg = $('#message').val();
+    $('div').append('<h2>' + msg + '</h2>');
+
+    });
+  });
